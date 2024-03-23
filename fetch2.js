228af0c4be3c337b23da90533ce18edebb4e7076
debu100 <debajitchakraborty96@gmail.com>
2024-03-23 10:35:25 +0530

    creating and upadating server data

--- fetch2.js
+++ fetch2.js
@@ -75,3 +75,36 @@ handlePromsie2()
   .catch((err) => {
     console.log(`error ${err}`);
   });
+
+//? Creating a resource
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+//?Updating a resource
+
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "PUT",
+  body: JSON.stringify({
+    id: 1,
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
